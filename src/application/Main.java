4f71757ada21ba07a7ac574aa2cc3e0c54e5fb27
Pedro Vitorino <pedrolucasofici@gmail.com>
2025-06-15 00:00:06 -0300

    SellerDaoJDBC and DaoFactory

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,5 +1,8 @@
 package application;
 
+import dao.DaoFactory;
+import dao.SellerDao;
+import dao.impl.SellerDaoJDBC;
 import entities.Department;
 import entities.Seller;
 
@@ -12,6 +15,8 @@ public class Main {
         Department obj = new Department(1, "Books");
 
         Seller seller = new Seller (21, "Bob", "bob@gmail.com", new Date(), 3000.0, obj);
+
+        SellerDao sellerDao = DaoFactory.createSellerDao();
 
         System.out.println(seller);
     }
---
+++ src/dao/impl/SellerDaoJDBC.java
@@ -0,0 +1,33 @@
+package dao.impl;
+
+import dao.SellerDao;
+import entities.Seller;
+
+import java.util.List;
+
+public class SellerDaoJDBC implements SellerDao {
+    @Override
+    public void insert(Seller obj) {
+
+    }
+
+    @Override
+    public void update(Seller obj) {
+
+    }
+
+    @Override
+    public void deleteById(Seller obj) {
+
+    }
+
+    @Override
+    public Seller findById(Integer obj) {
+        return null;
+    }
+
+    @Override
+    public List<Seller> findAll() {
+        return List.of();
+    }
+}
